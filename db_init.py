33fa56ccaed06bba91aab5ccbdde4402748236ca
frontc <fangchao@lefer.cn>
2017-08-06 08:34:34 +0800

    调整依赖包，修改初始化语句

--- db_init.py
+++ db_init.py
@@ -8,7 +8,7 @@ db = pymysql.connect(host='localhost',user='root',passwd='odsods',db='pm',charse
 cu = db.cursor()
 cu.execute("delete from user")
 db.commit()
-for row in [('方超','d2814','fangchao@tydic.com','18956060302','ods','0','1'),('朱丙省','d0924','zhubs@tydic.com','15375346803','123456','1','1')]:
+for row in [('方超','d2814','fangchao@tydic.com','15375346803','ods','0','1'),('朱丙省','d0924','zhubs@tydic.com','15375346803','123456','1','1')]:
     #print("insert into user (user_name,staff_id,email,phone_number,password,role) values('{0}','{1}','{2}','{3}','{4}')".format(*row))
     cu.execute("insert into user (user_name,staff_id,email,phone_number,password,role,enable) values('{0}','{1}','{2}','{3}','{4}','{5}','{6}')".format(*row))
 db.commit()
